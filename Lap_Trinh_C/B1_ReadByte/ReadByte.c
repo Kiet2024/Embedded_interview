1f64d9ec017667731a9e065b0bb10db111e5c365
Kiet2024 <nguyenkiet19122000@gmail.com>
2023-05-17 16:51:44 +0700

    update

--- Lap_Trinh_C/B1_ReadByte/ReadByte.c
+++ Lap_Trinh_C/B1_ReadByte/ReadByte.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
 
+void readByte(uint8_t byte){
+    uint8_t temp = 0b10000000;
+    uint8_t result;
+    printf("0b");
+    for (int i = 0; i < 8; i++)
+    {
+        result = byte & temp ;
+        if (result == 0)
+        {
+            printf("0");    
+        }
+        else{
+            printf("1");
+        }
+        byte = byte << 1; 
+    }
+}
 
 int main()
 {
-    printf("hello worldgggg");
-
+    uint8_t test = 0b10010001;
+    readByte(test);
+    return 0;
 }
