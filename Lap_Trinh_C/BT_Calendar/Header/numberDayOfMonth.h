9c71e668ffe58a6a224b5106fc9f5af12dfc0adb
Kiet2024 <nguyenkiet19122000@gmail.com>
2023-06-09 09:51:11 +0700

    update

--- Lap_Trinh_C/BT_Calendar/Header/numberDayOfMonth.h
+++
@@ -1,6 +0,0 @@
-#ifndef __numberDayOfMonth_H
-#define __numberDayOfMonth_H
-
-int numberDayOfMonth (int month, int year);
-
-#endif
--- Lap_Trinh_C/BT_Calendar/Source/main.c
+++ Lap_Trinh_C/BT_Calendar/Source/main.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
-#include "numberDayOfMonth.h"
+#include "calculateDayOfWeek.h"
+
 
 int main()
 {
-    int day, month, year, weekday;
+
+    int day, month, year, totalDays;
     do
     {
-        printf("Moi nhap ngay thang nam hop le: ");
+        printf("Please enter a valid date: ");
         scanf("%d%d%d",&day, &month, &year);
-    } while (day < 1 || day > numberDayOfMonth(month, year) || month < 1 || month > 12 || year < 1 );
+    } while (day < 1 || day > calculateNumberDayOfMonth(month, year) || month < 1 || month > 12 || year < 1 );
+   totalDays = calculateNumberOfDays(day, month, year);
+   dayOfWeek(totalDays);
 
-    printf("Ngay %d Thang %d Nam %d la ",day, month, year);
-    year -= (14 - month)/12;
-    month += 12*((14 - month)/12) - 2;
-    weekday = (day + year + year/4 - year/100 + year/400 + (31 * month)/12)%7;
-    if (weekday == 0) {
-        printf("chu nhat\n");
-    }
-    else{
-        printf("thu %d\n ",weekday + 1);
-    }
+   return 0;
 
 }
+
+
+   /*
+    //Tìm thứ của một ngày bằng công thức
+        printf("Ngay %d Thang %d Nam %d la ",day, month, year);
+        year -= (14 - month)/12;
+        month += 12*((14 - month)/12) - 2;
+        weekday = (day + year + year/4 - year/100 + year/400 + (31 * month)/12)%7;
+        if (weekday == 0) {
+            printf("chu nhat\n");
+        }
+        else{
+            printf("thu %d\n ",weekday + 1);
+        }
+    */
